b85302ebad005a54d94d443fcdc2886e92fab0c2
Vadercasper <danielayedegbe1@gmail.com>
2022-11-06 13:26:14 +0100

    added beep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from tkinter import *
 import math
+import winsound
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -7,11 +8,12 @@ GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 GRIN = "#395144"
 FONT_NAME = "Courier"
-WORK_MIN = 1
-SHORT_BREAK_MIN = 0.25
-LONG_BREAK_MIN = 0.5
-reps = 0
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 30
+reps = 5
 timer = None
+
 # ---------------------------- TIMER RESET ------------------------------- # 
 def reset():
     global reps
@@ -27,6 +29,7 @@ def start_timer():
     work_secs = WORK_MIN * 60
     short_break_secs = SHORT_BREAK_MIN * 60
     long_break_secs = LONG_BREAK_MIN * 60
+    check_mark.config(text="")
     if reps % 8 == 0:
         count_down(long_break_secs)
         title_label.config(text="Break", fg=RED)
@@ -36,11 +39,12 @@ def start_timer():
     else:
         count_down(work_secs)
         title_label.config(text="Work", fg=GREEN)
+    winsound.Beep(500, 1000)
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 def count_down(count):
     global timer
     minutes = math.floor(count / 60)
-    seconds = count % 60
+    seconds = int(count % 60)
     if seconds < 10:
         seconds = f'0{seconds}'
     if seconds == 0:
@@ -61,8 +65,7 @@ window.title("Pomodoro")
 tomato = PhotoImage(file="tomato.png")
 window.config(padx=100, pady=50, bg=YELLOW)
 
-
-
+window.iconbitmap('icon.ico')
 title_label = Label(text="Timer", fg=GREEN, font=(FONT_NAME, 50), bg=YELLOW)
 title_label.grid(column=1, row=0)
 
@@ -76,7 +79,7 @@ canvas.grid(column=1, row=1)
 start_button = Button(text="start", bg=YELLOW, fg=GRIN, command=start_timer, highlightthickness=0)
 start_button.grid(column=0, row=2)
 
-reset_button = Button(text="reset", bg=YELLOW, fg=GRIN, command = reset, highlightthickness=0)
+reset_button = Button(text="reset", bg=YELLOW, fg=GRIN, command=reset, highlightthickness=0)
 reset_button.grid(column=2, row=2)
 
 check_mark = Label(text="✓", bg=YELLOW, fg=GREEN, font=(FONT_NAME, 30))
